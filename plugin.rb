ccad3ca226618fd6f233aee5eb3e8156c7c5eed2
Penar Musaraj <pmusaraj@gmail.com>
2019-02-04 16:14:43 -0500

    FEAT: ads.txt support
    Adds a plugin setting that holds text content served on a new /ads.txt route.

--- plugin.rb
+++ plugin.rb
@@ -5,3 +5,20 @@
 # url: https://github.com/discourse/discourse-adplugin
 
 register_asset "stylesheets/adplugin.scss"
+
+after_initialize do
+  require_dependency 'application_controller'
+  class ::AdstxtController < ::ApplicationController
+    skip_before_action :check_xhr
+
+    def index
+      raise Discourse::NotFound unless SiteSetting.adsense_ads_txt.present?
+
+      render plain: SiteSetting.adsense_ads_txt
+    end
+  end
+
+  Discourse::Application.routes.append do
+    get '/ads.txt' => "adstxt#index"
+  end
+end
