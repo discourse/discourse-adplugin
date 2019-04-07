a1649f7ccefbc836c42ec98fe5c5b3deb4ac88c6
Diego Barreiro <diego.barreiro.perez@gmail.com>
2019-04-07 15:55:55 +0200

    BUMP: Plugin Version

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-adplugin
 # about: Ad Plugin for Discourse
-# version: 1.0.2
+# version: 1.1.0
 # authors: Vi and Sarah (@ladydanger and @cyberkoi)
 # url: https://github.com/discourse/discourse-adplugin
 
