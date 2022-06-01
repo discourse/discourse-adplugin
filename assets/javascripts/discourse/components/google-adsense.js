e998ac3e0bb3ec3b631b57d96181684d32ebecc4
Isaac Janzen <isaac.janzen@discourse.org>
2022-06-01 10:55:21 -0500

    DEV: Remove 'htmlSafe' string prototype extensions
    lint

--- assets/javascripts/discourse/components/google-adsense.js
+++ assets/javascripts/discourse/components/google-adsense.js
@@ -4,6 +4,7 @@ import loadScript from "discourse/lib/load-script";
 import RSVP from "rsvp";
 import { scheduleOnce } from "@ember/runloop";
 import { isTesting } from "discourse-common/config/environment";
+import { htmlSafe } from "@ember/template";
 
 let _loaded = false,
   _promise = null,
@@ -191,28 +192,24 @@ export default AdComponent.extend({
 
   @discourseComputed("placement", "showAd")
   classForSlot(placement, showAd) {
-    return showAd ? `adsense-${placement}`.htmlSafe() : "";
+    return showAd ? htmlSafe(`adsense-${placement}`) : "";
   },
 
   @discourseComputed("isResponsive", "isFluid")
   autoAdFormat(isResponsive, isFluid) {
-    return isResponsive
-      ? isFluid
-        ? "fluid".htmlSafe()
-        : "auto".htmlSafe()
-      : false;
+    return isResponsive ? htmlSafe(isFluid ? "fluid" : "auto") : false;
   },
 
   @discourseComputed("ad_width", "ad_height", "isResponsive")
   adWrapperStyle(w, h, isResponsive) {
-    return (isResponsive ? "" : `width: ${w}; height: ${h};`).htmlSafe();
+    return htmlSafe(isResponsive ? "" : `width: ${w}; height: ${h};`);
   },
 
   @discourseComputed("adWrapperStyle", "isResponsive")
   adInsStyle(adWrapperStyle, isResponsive) {
-    return `display: ${
-      isResponsive ? "block" : "inline-block"
-    }; ${adWrapperStyle}`.htmlSafe();
+    return htmlSafe(
+      `display: ${isResponsive ? "block" : "inline-block"}; ${adWrapperStyle}`
+    );
   },
 
   @discourseComputed("currentUser.trust_level")
