discourse/discourse-adplugin

commit 0948d6b19e34320f95226cc43fbce976b71ceb31

DEV: Refactor initializer to use RenderGlimmer and registerWidgetShim

assets/javascripts/discourse/initializers/initialize-ad-plugin.js
@@ -1,40 +1,30 @@
+import { hbs } from "ember-cli-htmlbars";
 import { withPluginApi } from "discourse/lib/plugin-api";
 import Site from "discourse/models/site";
+import { registerWidgetShim } from "discourse/widgets/render-glimmer";
 
 export default {
   name: "initialize-ad-plugin",
   initialize(container) {
+    registerWidgetShim(
+      "after-post-ad",
+      "div.widget-connector",
+      hbs`<PostBottomAd @model={{@data}} />`
+    );
+
     withPluginApi("0.1", (api) => {
-      api.decorateWidget("post:after", (dec) => {
-        if (dec.canConnectComponent) {
-          if (!dec.attrs.cloaked) {
-            return dec.connect({
-              component: "post-bottom-ad",
-              context: "model",
-            });
-          }
-        } else {
-          // Old way for backwards compatibility
-          return dec.connect({
-            templateName: "connectors/post-bottom/discourse-adplugin",
-            context: "model",
-          });
-        }
+      api.decorateWidget("post:after", (helper) => {
+        return helper.attach("after-post-ad", helper.attrs);
       });
     });
 
-    const messageBus = container.lookup("message-bus:main");
-    if (!messageBus) {
-      return;
-    }
-
+    const messageBus = container.lookup("service:message-bus");
     const currentUser = container.lookup("service:current-user");
-    let channel;
-    if (currentUser) {
-      channel = "/site/house-creatives/logged-in";
-    } else {
-      channel = "/site/house-creatives/anonymous";
-    }
+
+    const channel = currentUser
+      ? "/site/house-creatives/logged-in"
+      : "/site/house-creatives/anonymous";
+
     messageBus.subscribe(channel, function (houseAdsSettings) {
       Site.currentProp("house_creatives", houseAdsSettings);
     });

commit ac72a3f7752d6f2c18ddf8f378d45432de56d0d0

DEV: Pass `post` instead of `transformedPost` to post-bottom-ad
Changes in 0948d6b19e34320f95226cc43fbce976b71ceb31 caused the post-bottom-ad component to start receiving the widget-ised 'transformedPost' instead of the original `post` model. In most cases this difference didn't matter, but it did cause noticable issues with `@model.category` and `@model.topic`.

This commit also deletes the unused `post-bottom/discourse-adplugin.hbs` connector. In the past, the adplugin itself was defining the post-bottom outlet. But now, we use RenderGlimmer to load the `post-bottom-ad` directly.

assets/javascripts/discourse/connectors/post-bottom/discourse-adplugin.hbs
@@ -1,1 +1,0 @@
-{{post-bottom-ad model=this}}

assets/javascripts/discourse/initializers/initialize-ad-plugin.js
@@ -14,7 +14,7 @@ export default {
 
     withPluginApi("0.1", (api) => {
       api.decorateWidget("post:after", (helper) => {
-        return helper.attach("after-post-ad", helper.attrs);
+        return helper.attach("after-post-ad", helper.widget.model);
       });
     });
 